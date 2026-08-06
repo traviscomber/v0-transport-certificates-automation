fc6fc178e2c2a1a4e804d8ad557f6f5f50822f00
traviscomber <n3uralia@gmail.com>
2026-08-06 15:59:03 -0400

    fix: externalize native canvas bindings

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,18 @@ const nextConfig = {
   },
   webpack(config, { isServer }) {
     if (isServer) {
-      config.externals.push('@napi-rs/canvas')
+      config.externals.push(({ request }, callback) => {
+        if (
+          request === '@napi-rs/canvas' ||
+          request?.startsWith('@napi-rs/canvas-')
+        ) {
+          return callback(null, `commonjs ${request}`)
+        }
+
+        return callback()
+      })
     }
+
     return config
   },
 }
